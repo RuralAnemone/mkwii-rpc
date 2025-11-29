9d986d7fe1ec30d56ffc3ac73c5d1bc09c141606
Isaiah W <isaiah.wallerstedt@proton.me>
2025-11-29 12:28:24 -0600

    implement severity and naming the logger

--- logger.js
+++ logger.js
@@ -13,13 +13,9 @@ export class Logger {
 ║                                                                   ║
 ╚═══════════════════════════════════════════════════════════════════╝`
 
-	constructor(verbosity = false) {
+	constructor(whoami, verbosity = false) {
 		this.isVerbose = verbosity;
-	}
-
-	clear() {
-		// stub for later functionality?
-		console.clear();
+		this.whoami = whoami;
 	}
 
 	showHeader() {
@@ -27,15 +23,45 @@ export class Logger {
 	}
 
 	init() {
-		// this.clear();
+		// stub for later functionality?
 		this.showHeader();
 	}
 
-	log(text, necessary) {
+	/**
+	 * Log a message to the console with a timestamp and its severity
+	 * @param {String} text text to log
+	 * @param {{"INFO"|"WARN"|"ERROR"}} severity log level (INFO, WARN, ERROR)
+	 */
+	log(text, severity) {
+		const now = new Date();
+		
+		// "ah yes let's have only one implementation of ISO8601, nobody's ever going to use any other format specified in the international standard."
+		// – ECMA
+		const timestamp = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.${now.getMilliseconds()}`;
+
+		let severityText = "";
+
+		switch (severity) {
+			case "INFO":
+				severityText = chalk.cyan("INFO ");
+				break;
+			case "WARN":
+				severityText = chalk.yellow("WARN ");
+				break;
+			case "ERROR":
+				severityText = chalk.bgRed.white("ERROR")
+				break;
+			default:
+				severityText = "     ";
+				break;
+		}
+
+		const formattedText = `${timestamp} ${severityText} [${this.whoami}] ${text}`
+
 		if (necessary) {
-			console.log(text)
+			console.log(formattedText)
 		} else if (this.isVerbose) {
-			console.log(text);
+			console.log(formattedText);
 		}
 	}
 }
